e93fc59e9b48092bc526466ba3b6f47f38f96d43
kaka <1397393964@qq.com>
2020-03-10 23:21:38 +0800

    基本数据类型

---
+++ 基本类型.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Printf("数据类型是:%T\n", 123)
+	fmt.Printf("数据类型是:%T\n", 123.4)
+	fmt.Printf("数据类型是:%T\n", "咔咔博客blog.fangkang.top")
+	fmt.Printf("数据类型是:%T\n", false)
+}
